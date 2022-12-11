10490dafe26dd654e0fedc7d3d0f37b314b0af0a
Aidan King <aidan.king@verizonmedia.com>
2022-12-11 17:00:38 +0000

    day-02 before testing part 1 against input

--- src/day-02/index.ts
+++ src/day-02/index.ts
@@ -1,19 +1,44 @@
 import { open } from 'node:fs/promises';
 import { getPathToTestFile } from '../helpers.js';
 
-const dataOne = await readMatchDataFromFile('test.txt');
+interface MatchResult {
+  winner: 'A' | 'B' | 'C' | 'X' | 'Y' | 'Z' | 'DRAW';
+  points: number;
+}
 
-console.log(dataOne);
+type MatchPairing =
+  | 'AX'
+  | 'AY'
+  | 'AZ'
+  | 'BX'
+  | 'BY'
+  | 'BZ'
+  | 'CX'
+  | 'CY'
+  | 'CZ';
 
-async function readMatchDataFromFile(fileName: string): Promise<string[]> {
-  const data: string[] = [];
+type MatchResults = {
+  [key in MatchPairing]: MatchResult;
+};
+
+const matchPairingsOne: MatchPairing[] = await readMatchDataFromFile(
+  'test.txt'
+);
+
+console.log(getTotalPointsForMe(matchPairingsOne));
+
+async function readMatchDataFromFile(
+  fileName: string
+): Promise<MatchPairing[]> {
+  const data: MatchPairing[] = [];
   try {
-    const pathToFile = getPathToTestFile('day-02', 'test.txt');
+    const pathToFile: string = getPathToTestFile('day-02', fileName);
+    console.log(pathToFile);
 
     const file = await open(pathToFile);
 
     for await (const line of file.readLines()) {
-      const id: string = line.split(' ').join('');
+      const id: MatchPairing = line.split(' ').join('') as MatchPairing;
       data.push(id);
     }
 
@@ -23,14 +48,34 @@ async function readMatchDataFromFile(fileName: string): Promise<string[]> {
   }
 }
 
-const matchResults = {
-  AX: { winner: 'DRAW', points: 4 },
-  AY: { winner: 'Y', points: 8 },
-  AZ: { winner: 'A', points: 7 },
-  BX: {},
-  BY: {},
-  BZ: {},
-  CX: {},
-  CY: {},
-  CZ: {},
-};
+function getTotalPointsForMe(matchPairings: MatchPairing[]) {
+  const matchResults: MatchResults = {
+    AX: { winner: 'DRAW', points: 3 },
+    AY: { winner: 'Y', points: 8 },
+    AZ: { winner: 'A', points: 7 },
+    BX: { winner: 'B', points: 8 },
+    BY: { winner: 'DRAW', points: 3 },
+    BZ: { winner: 'Z', points: 9 },
+    CX: { winner: 'X', points: 7 },
+    CY: { winner: 'C', points: 9 },
+    CZ: { winner: 'DRAW', points: 3 }
+  };
+
+  let totalPointsForMe = 0;
+
+  matchPairingsOne.forEach((mathPairing: MatchPairing): void => {
+    const matchResult: MatchResult = matchResults[mathPairing];
+    const winner = matchResult.winner;
+    if (
+      winner === 'X' ||
+      winner === 'Y' ||
+      winner === 'Z' ||
+      winner === 'DRAW'
+    ) {
+      totalPointsForMe += matchResult.points;
+    }
+  });
+
+  return totalPointsForMe;
+}
+
